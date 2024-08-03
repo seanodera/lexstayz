461d9d9e14c70403a1b85b99ddeb21d9b2f280cf
seanolero <seanolero@outlook.com>
2024-08-03 23:51:56 +0300

    Enhance date picker and guest selection responsiveness
    Added media queries for date picker UI adjustments on mobile devices. Integrated `react-responsive` to dynamically update date picker modes and utilized the new package to handle diverse browser environments. Minor refactoring for consistent formatting and improved readability.

--- src/components/stay/stayDate.tsx
+++ src/components/stay/stayDate.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import {useAppDispatch, useAppSelector} from "@/hooks/hooks";
 import {Button, DatePicker, InputNumber} from "antd";
 import {RangePickerProps} from "antd/es/date-picker";
@@ -8,53 +7,60 @@ import {selectConfirmBooking, updateBookingData} from "@/slices/confirmBookingSl
 import {Popover, PopoverButton, PopoverPanel} from "@headlessui/react";
 import {MdPersonOutline} from "react-icons/md";
 import {useEffect, useState} from "react";
+import {useMediaQuery} from 'react-responsive';
+
 
 const { RangePicker } = DatePicker;
 
 export default function StayDate() {
     const dispatch = useAppDispatch();
-    const booking = useAppSelector(selectConfirmBooking)
+    const booking = useAppSelector(selectConfirmBooking);
     const [numGuests, setNumGuests] = useState(booking.numGuests);
     const [startDate, setStartDate] = useState(booking.checkInDate);
     const [endDate, setEndDate] = useState(booking.checkOutDate);
 
+    const isMobile = useMediaQuery({ maxWidth: 768 });
 
     const disabledDate: RangePickerProps['disabledDate'] = (current) => {
         // Can not select days before today and today
         return current < dayjs().subtract(1,'day').endOf('day');
     };
+
     useEffect(() => {
         dispatch(updateBookingData({
             numGuests: numGuests,
             checkInDate: startDate,
             checkOutDate: endDate,
         }))
-    }, [numGuests,startDate,endDate])
+    }, [numGuests,startDate,endDate]);
+
     return (
         <div className="bg-primary-50 rounded-2xl p-4 my-4 shadow-md">
             <div className="flex max-md:flex-col gap-4">
-                <div className="">
+                <div>
                     <RangePicker
-                    disabledDate={disabledDate}
-                    size={'large'}
-                    format={'DD MMMM YYYY'}
-                    className={'border border-gray-500 rounded-xl py-2 px-3 bg-transparent'}
-                    value={[dayjs(booking.checkInDate),dayjs(booking.checkOutDate)]}
-                    onChange={(value) => {
-                    if (value) {
-
-                        setStartDate(dayjs(value[0]).toString())
-                        setEndDate(dayjs(value[1]).toString())
-                    }
-                }}
+                        disabledDate={disabledDate}
+                        size={'large'}
+                        format={'DD MMMM YYYY'}
+                        className={'border border-gray-500 rounded-xl py-2 px-3 bg-transparent'}
+                        value={[dayjs(booking.checkInDate), dayjs(booking.checkOutDate)]}
+                        onChange={(value) => {
+                            if (value) {
+                                setStartDate(dayjs(value[0]).toString());
+                                setEndDate(dayjs(value[1]).toString());
+                            }
+                        }}
+                        mode={isMobile ? ['date', 'date'] : undefined} // Show only one month on mobile
                     />
-
                 </div>
-                <Popover className="relative" >
+                <Popover className="relative">
                     <PopoverButton
                         as={Button}
-                        size={'large'} className={'bg-transparent text-current max-md:rounded-lg border-black w-max'}>
-                        <MdPersonOutline className={'text-lg'} /> {numGuests} Guests</PopoverButton>
+                        size={'large'}
+                        className={'bg-transparent text-current max-md:rounded-lg border-black w-max'}
+                    >
+                        <MdPersonOutline className={'text-lg'} /> {numGuests} Guests
+                    </PopoverButton>
 
                     <PopoverPanel anchor="bottom" className="flex flex-col p-4 z-20 shadow-xl rounded-xl ">
                         <div className={'grid grid-cols-2 bg-white items-center justify-center rounded-xl gap-2 px-8 py-4'}>
@@ -67,7 +73,6 @@ export default function StayDate() {
                                     min={1}
                                 />
                             </div>
-
                         </div>
                     </PopoverPanel>
                 </Popover>
