10c2be35f5c109cc64c14275c991d41331812917
seanolero <seanolero@outlook.com>
2024-08-03 23:31:05 +0300

    Update cart to remove items not matching stay ID
    Modified the `Stay` component to ensure the cart is updated by removing items that do not match the current stay ID. Improved code readability by removing extraneous spaces.

--- src/app/stay/[id]/page.tsx
+++ src/app/stay/[id]/page.tsx
@@ -1,11 +1,9 @@
 'use client'
 
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
-import { useParams } from "next/navigation";
-import { useAppDispatch, useAppSelector } from "@/hooks/hooks";
-
-
+import {useParams} from "next/navigation";
+import {useAppDispatch, useAppSelector} from "@/hooks/hooks";
 
 
 import Banner from "@/components/stay/banner";
@@ -17,21 +15,28 @@ import CartSummary from "@/components/stay/cartSummary";
 import FeaturedRoom from "@/components/stay/featuredRoom";
 import AvailableRooms from "@/components/stay/availableRooms";
 import {selectCurrentId, selectCurrentStay, setCurrentStayFromId} from "@/slices/staysSlice";
+import {selectCart, updateCart} from "@/slices/bookingSlice";
 
 
 export default function Stay() {
-    const params = useParams()['id'];
+    const params = useParams()[ 'id' ];
     const dispatch = useAppDispatch();
     const currentId = useAppSelector(selectCurrentId);
+    const cart = useAppSelector(selectCart)
 
     useEffect(() => {
 
-            dispatch(setCurrentStayFromId(params.toString()));
-
+        dispatch(setCurrentStayFromId(params.toString()));
+        let newCart = [...cart];
+        newCart.forEach((value: any, index: number) => {
+            if (value.stayId !== params.toString()) {
+                newCart.splice(index, 1)
+            }
+        })
+        dispatch(updateCart(newCart));
     }, [dispatch, params]);
 
     const stay = useAppSelector(selectCurrentStay);
-
 
 
     if (!stay || stay.id === undefined) {
@@ -39,20 +44,20 @@ export default function Stay() {
     } else {
         return (
             <div className="lg:px-24 px-7 py-24 bg-white text-dark">
-                <Banner stay={stay} />
+                <Banner stay={stay}/>
                 <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 pt-8">
                     <div className="col-span-2">
-                        <Details stay={stay} />
-                        <Description stay={stay} />
-                        <StayDate />
-                        <AvailableRooms stay={stay} />
+                        <Details stay={stay}/>
+                        <Description stay={stay}/>
+                        <StayDate/>
+                        <AvailableRooms stay={stay}/>
                     </div>
                     <div className="max-lg:hidden lg:ps-12 col-span-1 md:col-span-2 lg:col-span-1">
-                        <FeaturedRoom stay={stay} />
-                        <CartSummary stay={stay} />
+                        <FeaturedRoom stay={stay}/>
+                        <CartSummary stay={stay}/>
                     </div>
                 </div>
-                <MobileCartSummary />
+                <MobileCartSummary/>
             </div>
         );
     }
