98e5452cfe4c19a10f1fd686a5769e2885909b12
seanolero <seanolero@outlook.com>
2024-08-09 03:32:00 +0300

    Refactor RoomComponent and handle unavailable rooms
    Refactor the RoomComponent function to clean up the code and improve state management. Added logic to reset the number of rooms when the room is unavailable, ensuring consistent behavior.

--- src/components/Grid Items/roomComponent.tsx
+++ src/components/Grid Items/roomComponent.tsx
@@ -3,16 +3,15 @@ import {IoBedOutline} from "react-icons/io5";
 import {LuBedSingle} from "react-icons/lu";
 import ReservationDialog from "@/components/stay/reservationDialog";
 import {useEffect, useState} from "react";
-import {Dialog, DialogPanel, DialogTitle, Select} from "@headlessui/react";
+import {Select} from "@headlessui/react";
 import {useAppDispatch, useAppSelector} from "@/hooks/hooks";
 import {selectCart, updateCart} from "@/slices/bookingSlice";
-import {Button, Card, Divider, Image} from "antd";
+import {Button, Divider, Image} from "antd";
 
 
 export default function RoomComponent({room, stay, className = '', available = true, lowest = 10}: { room: any, stay: any, className?: string, available?: boolean, lowest?: number }) {
     const dispatch = useAppDispatch();
     const globalCart = useAppSelector(selectCart)
-    const fullState = useAppSelector((state: any) => state.booking);
     const [numRooms, setNumRooms] = useState<number>(0);
     const [open, setOpen] = useState(false);
     const [roomIndex, setRoomIndex] = useState<number>(-1);
@@ -26,41 +25,50 @@ export default function RoomComponent({room, stay, className = '', available = t
 
     }, [globalCart])
 
-    function handleCart() {
-        // Clone the global cart array to avoid mutating the original array
-        let newCart = [...globalCart];
-
-        // Check if the number of rooms is greater than zero
-        if (numRooms > 0) {
-            // Check if the room is already in the cart by its index
-            if (roomIndex !== -1) {
-                // Update the existing room entry in the cart
-                newCart[roomIndex] = {
-                    name: room.name,
-                    price: room.price,
-                    numRooms: numRooms,
-                    roomId: room.id,
-                    stayId: stay.id
-                };
-            } else {
-                // Add a new room entry to the cart
-                newCart.push({
-                    name: room.name,
-                    price: room.price,
-                    numRooms: numRooms,
-                    roomId: room.id,
-                    stayId: stay.id
-                });
-            }
-        } else if (numRooms === 0 && roomIndex !== -1) {
-            // Remove the room from the cart if the number of rooms is zero
-            newCart.splice(roomIndex, 1);
-        }
-
-        // Dispatch the updated cart
-        dispatch(updateCart(newCart));
-    }
     useEffect(() => {
+        if (!available){
+            setNumRooms(0);
+        } else if (lowest > numRooms){
+            setNumRooms(lowest)
+        }
+    }, [lowest, available]);
+
+
+    useEffect(() => {
+        function handleCart() {
+            // Clone the global cart array to avoid mutating the original array
+            let newCart = [...globalCart];
+
+            // Check if the number of rooms is greater than zero
+            if (numRooms > 0) {
+                // Check if the room is already in the cart by its index
+                if (roomIndex !== -1) {
+                    // Update the existing room entry in the cart
+                    newCart[roomIndex] = {
+                        name: room.name,
+                        price: room.price,
+                        numRooms: numRooms,
+                        roomId: room.id,
+                        stayId: stay.id
+                    };
+                } else {
+                    // Add a new room entry to the cart
+                    newCart.push({
+                        name: room.name,
+                        price: room.price,
+                        numRooms: numRooms,
+                        roomId: room.id,
+                        stayId: stay.id
+                    });
+                }
+            } else if (numRooms === 0 && roomIndex !== -1) {
+                // Remove the room from the cart if the number of rooms is zero
+                newCart.splice(roomIndex, 1);
+            }
+
+            // Dispatch the updated cart
+            dispatch(updateCart(newCart));
+        }
         handleCart()
     }, [numRooms])
 
@@ -107,110 +115,10 @@ export default function RoomComponent({room, stay, className = '', available = t
     </div>
 }
 
-export function RoomComponentOld({room, stay, className = ''}: { room: any, stay: any, className?: string }) {
-    const dispatch = useAppDispatch();
-    const globalCart = useAppSelector(selectCart)
-    const fullState = useAppSelector((state: any) => state.booking);
-    const [numRooms, setNumRooms] = useState<number>(0);
-    const [open, setOpen] = useState(false);
-    const [roomIndex, setRoomIndex] = useState<number>(-1);
-
-    useEffect(() => {
-        let _roomIndex = globalCart.findIndex((value: any) => value.roomId === room.id);
-        setRoomIndex(_roomIndex)
-        if (_roomIndex !== -1){
-            setNumRooms(globalCart[_roomIndex].numRooms)
-        }
-
-    }, [globalCart])
-
-    function handleCart() {
-        // Clone the global cart array to avoid mutating the original array
-        let newCart = [...globalCart];
-
-        // Check if the number of rooms is greater than zero
-        if (numRooms > 0) {
-            // Check if the room is already in the cart by its index
-            if (roomIndex !== -1) {
-                // Update the existing room entry in the cart
-                newCart[roomIndex] = {
-                    name: room.name,
-                    price: room.price,
-                    numRooms: numRooms,
-                    roomId: room.id,
-                    stayId: stay.id
-                };
-            } else {
-                // Add a new room entry to the cart
-                newCart.push({
-                    name: room.name,
-                    price: room.price,
-                    numRooms: numRooms,
-                    roomId: room.id,
-                    stayId: stay.id
-                });
-            }
-        } else if (numRooms === 0 && roomIndex !== -1) {
-            // Remove the room from the cart if the number of rooms is zero
-            newCart.splice(roomIndex, 1);
-        }
-
-        // Dispatch the updated cart
-        dispatch(updateCart(newCart));
-    }
-    useEffect(() => {
-        handleCart()
-    }, [numRooms])
-    return <div
-        className={`${(roomIndex !== -1) && 'shadow-primary'} ${className ? className : 'rounded-2xl p-4 shadow-md'}`}>
-        <div className={'flex justify-between items-center mb-1'}>
-            <h3 className={'text-xl font-medium mb-0'}>{room.name}</h3>
-            <div className={'flex justify-between'}> <span
-                className={'font-medium text-primary text-xl'}>{'$'} {room.price.toLocaleString(undefined, {
-                minimumFractionDigits: 2, maximumFractionDigits: 2
-            })} <span className={'font-light text-sm'}>/night</span></span></div>
-        </div>
-        <div className={'grid grid-cols-3'}>
-            <Image className={'aspect-video rounded-xl object-cover'} src={room.poster} alt={room.name}/>
-
-            <div>
-                <div className={'flex flex-col items-center gap-2 mb-4 overflow-x-scroll'}>
-                    {
-                        room.beds.map((bed: any, index: number) => <div key={index}
-                                                                        className={'text-center text-sm text-nowrap'}>
-                            {(bed.type.toLowerCase() === 'king' || bed.type.toLowerCase() === 'double') ?
-                                <IoBedOutline/> : <LuBedSingle/>} {bed.number} &bull; {bed.type} Bed{bed.number === 1? '' : 's'}
-                        </div>)
-                    }
-                </div>
-            </div>
-
-            <div className={'flex flex-col items-end gap-2'}>
-                <div className={'flex flex-col justify-end gap-2 w-max'}><Select value={numRooms}
-                                                                                 onChange={(e) => setNumRooms(parseInt(e.target.value))}
-                                                                                 className={'appearance-none rounded-xl border border-primary text-sm py-2 px-2 text-start bg-transparent'}>
-                    {Array.from({length: 11}, (_, i) => <option value={i}
-                                                                key={i}>{i} {(i === 1) ? 'Room' : 'Rooms'}</option>)}
-                </Select>
-                    <Button type={'primary'} size={'large'} className={''}
-                            onClick={() => setOpen(true)}>View Room
-                    </Button>
-                </div>
-            </div>
-        </div>
-
-
-
-
-        <ReservationDialog isOpen={open} setIsOpen={setOpen} room={room} stay={stay} numRoom={numRooms} setNumRoom={setNumRooms}/>
-    </div>
-}
-
 
 export function RoomComponentPortrait({room, stay, className = '', available = true, lowest = 10}: { room: any, stay: any, className?: string, available?: boolean, lowest?: number }) {
     const dispatch = useAppDispatch();
     const globalCart = useAppSelector(selectCart)
-    const fullState = useAppSelector((state: any) => state.booking);
     const [numRooms, setNumRooms] = useState<number>(0);
     const [open, setOpen] = useState(false);
     const [roomIndex, setRoomIndex] = useState<number>(-1);
@@ -223,6 +131,13 @@ export function RoomComponentPortrait({room, stay, className = '', available = t
         }
 
     }, [globalCart])
+    useEffect(() => {
+        if (!available){
+            setNumRooms(0);
+        } else if (lowest > numRooms){
+            setNumRooms(lowest)
+        }
+    }, [lowest, available]);
 
     function handleCart() {
         // Clone the global cart array to avoid mutating the original array
