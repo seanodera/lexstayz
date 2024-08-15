e0b1107ba1c126960052a2078bd44bc8ad9c717d
seanolero <seanolero@outlook.com>
2024-08-15 20:09:28 +0300

    Enhance description component with expandable text
    Added Ant Design's Typography component for better text handling, enabling expandable and collapsible descriptions. Incorporated useState to manage the expanded state and replaced the previous line-clamp implementation.

--- src/components/stay/description.tsx
+++ src/components/stay/description.tsx
@@ -1,7 +1,20 @@
+'use client'
+import {Typography} from "antd";
+import {useState} from "react";
+
 export default function Description({stay}: { stay: any }) {
+    const [expanded, setExpanded] = useState(false);
     return (
         <div className="py-4 border-t border-b border-gray-500">
-            <p className="max-lg:line-clamp-5">{stay.description}</p>
+            <Typography.Paragraph ellipsis={{
+                rows: 3,
+                expandable: 'collapsible',
+                expanded,
+                onExpand: (_, info) => setExpanded(info.expanded)
+            }}>
+                {stay.description}
+            </Typography.Paragraph>
+            {/*<p className="max-lg:line-clamp-5">{stay.description}</p>*/}
         </div>
     );
 }
