5b3b74d1f3144eb1562e085a873cb755a5e1d5ac
seanolero <seanolero@outlook.com>
2024-08-25 10:34:06 +0300

    Add dynamic map component and contact page
    Added dynamic MapWithMarker component to the stay page and a new contact page for user inquiries. Updated dependencies to include leaflet and react-leaflet for map functionality, and added new advertisement images and an SVG asset.

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function AboutUsPage() {
     return (
-        <div className="md:px-16 px-7 py-8 bg-gradient-to-t from-primary-100 via-primary-100 via-60% to-transparent ">
+        <div className="md:px-16 px-7 py-8 pt-[4.5rem] bg-gradient-to-t from-primary-100 via-primary-100 via-60% to-transparent ">
             <div className="grid grid-cols-3 gap-6 md:w-3/4">
                 <div className="px-7">
                     <Menu className="rounded-xl fixed ms-auto" items={[
